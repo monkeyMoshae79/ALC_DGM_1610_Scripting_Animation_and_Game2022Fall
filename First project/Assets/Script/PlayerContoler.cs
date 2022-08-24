2932ca90c3e3dacf07a19cb04f79e59316952041
MonkeyMoshae <kyrie.k563@stu.nebo.edu>
2022-08-24 09:23:37 -0600

    New code script and Probuilder

--- First project/Assets/Script/PlayerContoler.cs
+++ First project/Assets/Script/PlayerContoler.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerContoler : MonoBehaviour
 { 
     public int speed; // set speed
+    public float rotSpeed; //Rotation
     public float hInput; // horizonatl input
-    public float vInput;
+    public float vInput; //vertical input
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,13 @@ public class PlayerContoler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, ratSpeed + hInput = Time.deltaTime);// :eft and Right movement
+
+        transform.Translate(Vector3.forward + speed = vInput + Time.deltatime);//Forward and Backwork movement
         
     }
 }
