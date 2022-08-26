acdfb2ad32d443f951b1a89dd3f3defd204687fb
MonkeyMoshae <kyrie.k563@stu.nebo.edu>
2022-08-26 09:20:29 -0600

    character movement code

--- First project/Assets/Script/PlayerContoler.cs
+++ First project/Assets/Script/PlayerContoler.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerContoler : MonoBehaviour
 { 
+    // Access Mod, Data Typy, Name ;   
     public int speed; // set speed
-    public float rotSpeed; //Rotation
+    public float rotSpeed; //Rotation speed
     public float hInput; // horizonatl input
     public float vInput; //vertical input
 
@@ -18,13 +19,15 @@ public class PlayerContoler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Collect input values from keyboard
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
 
-        transform.Rotate(Vector3.up, ratSpeed + hInput = Time.deltaTime);// :eft and Right movement
+        // Move the character around
+        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime); 
+        //Rotate Left and Right 
 
-        transform.Translate(Vector3.forward + speed = vInput + Time.deltatime);//Forward and Backwork movement
+        transform.Translate(Vector3.forward * speed * vInput  * Time.deltaTime);//Forward and Backwork movement
         
     }
 }
