5d127c9e086d8cbb6159e77c0c1e0d7f1ad80e9c
MonkeyMoshae <kyrie.k563@stu.nebo.edu>
2022-09-29 09:18:56 -0600

    BebRouge: Movement and collisions

--- RougeBob/Assets/Script/PlayerController.cs
+++ RougeBob/Assets/Script/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed; // speed
+    private float hInput; //horixontal movement
+    private float vInput; //vertical movement
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * hInput * speed * Time.deltaTime);// move left and right
+       //Alt way write Vector2.right: Vector(speed,0)
+
+        transform.Translate(Vector2.up * vInput * speed * Time.deltaTime); // Move forward and back
+        //alt way write Vector2.up: Vector(0,speed)
+
     }
 }
