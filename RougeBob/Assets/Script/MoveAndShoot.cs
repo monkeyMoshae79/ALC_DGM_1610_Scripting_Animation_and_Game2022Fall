bdcee3649aae0c3cc36e377e5824ad4472078cf8
MonkeyMoshae <kyrie.k563@stu.nebo.edu>
2022-10-18 09:21:26 -0600

    Goblin move, Projectile shoot, fix door/key

---
+++ RougeBob/Assets/Script/MoveAndShoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndShoot : MonoBehaviour
+{
+    public float speed;
+    public float stopDistance;
+    public float retreatDistance;
+    private Transform target;
+    private float shotDelay;
+    public float startDelay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
